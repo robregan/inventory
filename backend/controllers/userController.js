326a8957a962f7ba5367d52d9cd9d72180d6e393
rob <robert_regan86@yahoo.com>
2022-10-27 12:14:55 -0400

    logged in status

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -136,9 +136,26 @@ const getUser = asyncHandler(async (req, res) => {
   }
 })
 
+// get logged in status
+
+const loginStatus = asyncHandler(async (req, res) => {
+  const token = req.cookies.token
+  if (!token) {
+    return res.json(false)
+  }
+
+  // verify token
+  const verified = jwt.verify(token, process.env.JWT_SECRET)
+  if (verified) {
+    return res.json(true)
+  }
+  return res.json(false)
+})
+
 module.exports = {
   registerUser,
   loginUser,
   logout,
   getUser,
+  loginStatus,
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,6 +4,7 @@ const {
   loginUser,
   logout,
   getUser,
+  loginStatus,
 } = require('../controllers/userController')
 const router = express.Router()
 const protect = require('../middleware/authMiddleware')
@@ -12,5 +13,6 @@ router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/logout', logout)
 router.get('/getuser', protect, getUser)
+router.get('/loggedin', loginStatus)
 
 module.exports = router
