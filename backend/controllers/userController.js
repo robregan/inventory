4469c09c072ba71748e69c7d03d216c44aa8783e
rob <robert_regan86@yahoo.com>
2022-10-27 11:27:01 -0400

    protected routes middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -117,8 +117,28 @@ const logout = asyncHandler(async (req, res) => {
   })
 })
 
+const getUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
 module.exports = {
   registerUser,
   loginUser,
   logout,
+  getUser,
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,11 +3,14 @@ const {
   registerUser,
   loginUser,
   logout,
+  getUser,
 } = require('../controllers/userController')
 const router = express.Router()
+const protect = require('../middleware/authMiddleware')
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/logout', logout)
+router.get('/getuser', protect, getUser)
 
 module.exports = router
