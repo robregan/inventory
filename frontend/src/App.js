a554be566539f3c98686c726853cf443743a5810
rob <robert_regan86@yahoo.com>
2022-10-29 09:41:18 -0400

    routes setup for auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import {
   Routes,
   Route,
 } from 'react-router-dom'
+import Forgot from './pages/auth/Forgot'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import Reset from './pages/auth/Reset'
 import Home from './pages/Home/Home'
 
 function App() {
@@ -11,6 +15,10 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot' element={<Forgot />} />
+        <Route path='/resetpassword/:resetToken' element={<Reset />} />
       </Routes>
     </Router>
   )
